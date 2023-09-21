3b7449a0ba9412579f66274299b0fa34c88f8d13
SirBaguette <sebastianz2026@student.cis.edu.hk>
2023-09-21 08:11:26 +0800

    .

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
+        int resultOne = add(1, 1);
+        String resultTwo = greeting("Baguette");
+        int resultThree = add(1,2,3,4);
 
+        System.out.println(resultOne);
+        System.out.println(resultTwo);
+        System.out.println(resultThree);
+
+        printMe("sosf");
+
+    }
+
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public static String greeting(String name) {
+        return "Bonjour, " + name +"!";
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        return add(add(numOne, numTwo), add(numThree, numFour));
+    }
+
+    public static void printMe(String toBePrinted) {
+        System.out.println(toBePrinted);
     }
 
 }
